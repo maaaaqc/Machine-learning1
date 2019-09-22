a7ec51f505c55811c402df576897b24faf739312
Qingchuan Ma <qingchuan.ma@mail.mcgill.ca>
2019-09-22 17:09:48 -0400

    Completed k-fold, low accuracy

--- step1.py
+++ step1.py
@@ -2,7 +2,7 @@ import numpy
 from pathlib import Path
 
 WINEDIR = Path.cwd() / "wine" / "winequality-red.csv"
-CANCERDIR = Path.cwd() / "wine" / "breast-cancer-wisconsin"
+CANCERDIR = Path.cwd() / "cancer" / "breast-cancer-wisconsin.data"
 
 def process_wine():
     red_wine = numpy.genfromtxt(str(WINEDIR), delimiter=";", skip_header=True)
@@ -17,14 +17,38 @@ def process_wine():
             row[-1] = 1
         else: 
             row[-1] = 0
+    for i in range(red_wine.shape[1]):
+        red_wine[:,i] = normalize(red_wine[:,i])
     return red_wine
 
 def process_cancer():
-    breast_cancer = numpy.genfromtxt(str(CANCERDIR), delimiter=",", skip_header=True)
+    breast_cancer = numpy.genfromtxt(str(CANCERDIR), delimiter=",", skip_header=False)[:,1:-1]
     index = 0
     for row in breast_cancer:
         if numpy.isnan(row).any():
             breast_cancer = numpy.delete(breast_cancer, index, 0)
         else:
             index += 1
+    for row in breast_cancer:
+        if row[-1] > 2:
+            row[-1] = 1
+        else: 
+            row[-1] = 0
+    for i in range(breast_cancer.shape[1]-1):
+        breast_cancer[:,i] = normalize(breast_cancer[:,i])
     return breast_cancer
+
+def normalize(column):
+    max = 0
+    min = float("inf")
+    for i in column:
+        if i > max:
+            max = i
+        if i < min:
+            min = i
+    for i in range(column.shape[0]):
+        column[i] = column[i] / (max - min)
+    return column
+
+if __name__ == "__main__":
+    process_wine()
--- step2.py
+++ step2.py
@@ -7,13 +7,13 @@ class LogReg:
         self.y = data[:,-1]
         self.x = data[:,0:-1]
 
-    def fit(self, x, y, rate, ites):
+    def fit(self, rate, ites):
         w = numpy.full((self.x.shape[1], 1), 1)
         for i in range(ites):
             w_old = w
-            for j in range(x.shape[0]):
-                sigma = self.sigma(numpy.matmul(w_old.transpose(), x[j].transpose())[0])
-                w = numpy.add(w, rate * ((y[j]-sigma) * x[j].reshape(x.shape[1],1)))
+            for j in range(self.x.shape[0]):
+                sigma = self.sigma(numpy.matmul(w_old.transpose(), self.x[j].transpose())[0])
+                w = numpy.add(w, rate * ((self.y[j]-sigma) * self.x[j].reshape(self.x.shape[1],1)))
         return w
 
     def sigma(self, ratio):
@@ -22,11 +22,35 @@ class LogReg:
         else:
             return 1 / (1 + math.exp(-ratio))
 
-    # def predict(data):
-    #    for :
+    def predict(self, val_set, w):
+        r = numpy.full((val_set.shape[0], 1), 0)
+        for i in range(val_set.shape[0]):
+            r[i] = numpy.matmul(w.transpose(), val_set[i])
+            if self.sigma(r[i]) >= 0.5:
+                r[i] = 0
+            else:
+                r[i] = 1
+        return r
+
+def kfold(data, k):
+    numpy.random.shuffle(data)
+    groups = numpy.array_split(data, k, axis=0)
+    for i in range(k):
+        val_set = groups[i][:,0:-1]
+        true_val = groups[i][:,-1]
+        train_set = numpy.concatenate(groups[:i] + groups[i+1:], axis=0)
+        model = LogReg(train_set)
+        w = model.fit(0.005, 1000)
+        r = model.predict(val_set, w)
+        print(get_accuracy(r, true_val))
+
+def get_accuracy(pred, fact):
+    count = 0
+    for i in range(pred.shape[0]):
+        if pred[i] == fact[i]:
+            count += 1
+    return float(count)/float(pred.shape[0])
 
 if __name__ == "__main__":
-    l1 = LogReg(step1.process_wine())
-    w  = l1.fit(l1.x, l1.y, 0.01, 1000)
-    print(w)
-
+    # kfold(step1.process_wine(), 5)
+    kfold(step1.process_cancer(), 5)
